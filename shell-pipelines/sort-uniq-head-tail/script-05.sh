4b506187728bdbbc9d297296b79edf8e97c0c749
Zhenishai <113972797+Zhenishai@users.noreply.github.com>
2026-04-02 07:34:56 +0200

    solve:sort-uniq-head-tail

--- shell-pipelines/sort-uniq-head-tail/script-05.sh
+++ shell-pipelines/sort-uniq-head-tail/script-05.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+sort events.txt | uniq
+
 # The input for this script is the events.txt file.
 # TODO: Write a command to show a list of all events that have happened, without duplication.
 # The order they're displayed doesn't matter, but we never want to see the same event listed twice.
